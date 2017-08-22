6e5ebf9ac283640d6755ca854ac37f8cef23568c
Furqan Freed <furqan@clicksco.com>
2017-08-22 15:16:55 +0400

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Google Analytics Client ID
+==========================
+
+##About
+Get Google Analytics Client ID
+
+
+##install
+**npm install @mae/ga_cid**   
+**npm ubpublish @mae/ga_cid**
+
+##Usage
+use google analytics or universal analytics to get client id.
+include this package and it will check of google analytics or universal analytics are inluced 
+then get client id, if didn't on first try retry couple of times and save as cookie, on form submit 
+for a lander we will get that google clientId
---
+++ ga_cid.js
@@ -0,0 +1,48 @@
+/**
+* Set Google Analytics Client Id in cooke and 
+* widow object to pass it with form post either get
+* it form javascript
+*/
+function setGaCid() {
+
+    var clientId = '';
+
+    //if directly set
+    ga(function(tracker) {
+        clientId = tracker.get('clientId');
+    });
+
+    //if not check if set with tag manager and have multiple then select first
+    if(clientId == '') {
+        ga(function(tracker) {
+            clientId = ga.getAll()[0].get('clientId');
+        });
+    }
+
+
+    //set cookie to get information in server side code and pass as attribute
+    document.cookie = "ga_client_id=" + clientId;
+    window.ga_client_id = clientId;
+}
+
+/**
+* Check Google Analytics Loaded
+* retry 5 times
+*/
+var counter = 1;
+function checkIfAnalyticsLoaded() {
+    if (window.ga) {
+        setGaCid();
+    } else {
+
+        //this counter set stack for this setTimeout
+        counter = counter + 1;
+        if (counter < 6){
+            setTimeout('checkIfAnalyticsLoaded()', 500);
+        } else {
+            setGaCid();
+            return;
+        }
+    }
+}
+window.onload = checkIfAnalyticsLoaded();
