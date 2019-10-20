c71039e94de7796567945d1f472ac718c7906826
Furqan Freed <furqan@clicksco.com>
2019-10-20 13:02:37 +0400

    show warning message if google analytics is not includeded

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ use google analytics or universal analytics to get client id.
 include this package and it will check of google analytics or universal analytics are inluced 
 then get client id, if didn't on first try retry couple of times and save as cookie, on form submit 
 for a lander we will get that google clientId
+
+
+Updates: exeptions added, if google analytics is not loaded then show warning message.
--- ga_cid.js
+++ ga_cid.js
@@ -1,10 +1,15 @@
 /**
-* Set Google Analytics Client Id in cooke and 
-* widow object to pass it with form post either get
-* it form javascript
-*/
+ * Set Google Analytics Client Id in cooke and
+ * widow object to pass it with form post either get
+ * it form javascript
+ */
 function setGaCid() {
-    
+
+    if(typeof ga == "undefined") {
+        console.warn('package @mae/gacid object ga is not defined.. include google analytics to get ga_client_id');
+        return;
+    }
+
     ga(function(tracker) {
         var clientId = '';
 
@@ -20,19 +25,26 @@ function setGaCid() {
 }
 
 /**
-* Check Google Analytics Loaded
-* retry 5 times
-*/
+ * Check Google Analytics Loaded
+ * retry 5 times
+ */
 var counter = 1;
 function checkIfAnalyticsLoaded() {
     if (window.ga) {
         setGaCid();
     } else {
-
         //this counter set stack for this setTimeout
         counter = counter + 1;
         if (counter < 6){
-            setTimeout('checkIfAnalyticsLoaded()', 500);
+            setTimeout(() => {
+                if(typeof checkIfAnalyticsLoaded == 'function') {
+                    checkIfAnalyticsLoaded()
+                }
+                else {
+                    console.warn('package @mae/gacid function checkIfAnalyticsLoaded is not defined!');
+                }
+
+            }, 500);
         } else {
             setGaCid();
             return;
